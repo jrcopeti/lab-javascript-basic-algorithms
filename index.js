8a1c0040929a135348679be37e0301f0c7499ab0
Jose Copeti <jrcopeti@gmail.com>
2024-06-19 14:32:21 +0200

    1 done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Iteration 1: Names and Input
-
-
+const hacker1 = "José";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Santa"
+console.log(`The driver's name is ${hacker2}`);
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
